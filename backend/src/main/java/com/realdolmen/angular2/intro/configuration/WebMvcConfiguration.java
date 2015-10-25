7de0e11e46ff055d69a4269a4154dd5c03df60b3
kustermj <johan.kustermans@realdolmen.com>
2015-10-26 00:21:49 +0100

    fixed compilation issues

--- backend/src/main/java/com/realdolmen/angular2/intro/configuration/WebMvcConfiguration.java
+++ backend/src/main/java/com/realdolmen/angular2/intro/configuration/WebMvcConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 @ComponentScan(value={"com.realdolmen.angular2.intro"})
-public class ServletApplicationContextConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
 	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
